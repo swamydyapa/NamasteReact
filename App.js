c7cb4da302d37df5d422c91da76394031f3c2d3e
swamydyapa <swamydyapa6.webdevp8@gmail.com>
2023-08-12 01:20:54 +0530

    created a component

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 // React Element
-const heading = <h1 className="heading">Helloo React🚀🚀</h1>;
+const heading = <h1 className="heading"> Helloo React🚀🚀</h1>;
 
 //  React Functional Component
 const HeadingComponent = () => {
-	return <h1 className="heading">Helloo React🚀🚀</h1>;
+	return <h1 className="heading">Helloo React Functional Component🚀🚀</h1>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(HeadingComponent);
-   
+root.render(<HeadingComponent/>);
