2baa586874b110a4435f41e4a4f53988712fd5c4
swamydyapa <swamydyapa6.webdevp8@gmail.com>
2023-08-12 01:16:18 +0530

    created a component

--- App.js
+++ App.js
@@ -1,29 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", { id: "parent" }, [
-	React.createElement("div", { id: "child" }, [
-		React.createElement("h1", {}, "This is Namaste React🚀🚀"),
-		React.createElement("h2", {}, "by Swamy Dyapa"),
-	]),
-	React.createElement("div", { id: "child2" }, [
-		React.createElement("h1", {}, "h1 from child2"),
-		React.createElement("h2", {}, "h2 from child2"),
-	]),
-]);
-console.log("reactElement", parent);
-const jsxParent = (
-	<div id="parent">
-		<div id="child">
-			<h1> Hello React🚀🚀</h1>
-			<h2>by Swamy Dyapa</h2>
-		</div>
-		<div id="child2" className="child2 child">
-			<h1>h1 from child2</h1>
-			<h2>h2 from child2</h2>
-		</div>
-	</div>
-);
-console.log("jsxElement", jsxParent);
+// React Element
+const heading = <h1 className="heading">Helloo React🚀🚀</h1>;
+
+//  React Functional Component
+const HeadingComponent = () => {
+	return <h1 className="heading">Helloo React🚀🚀</h1>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxParent);
+root.render(HeadingComponent);
+   
