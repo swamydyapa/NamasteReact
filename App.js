fd0de40503e2f157229355bff53ff2ff4749b5a2
swamydyapa <swamydyapa6.webdevp8@gmail.com>
2023-08-12 03:10:55 +0530

    Created a composition of components

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-// React Element
-const heading = <h1 className="heading"> Helloo React🚀🚀</h1>;
+const title = <h1 className="title"> Hello React 😍🚀</h1>;
 
-//  React Functional Component
+const FooterComponent = () => {
+	return <h1 className="footer">🚀I am a Footer🚀</h1>;
+};
+// can also place React components inside React element
+
+const footerElem = <div className="title"> <FooterComponent/> </div>;
+
 const HeadingComponent = () => {
-	return <h1 className="heading">Helloo React Functional Component🚀🚀</h1>;
+	return (
+		<div className="container">
+			{title}
+			<h1 className="heading">Heading Component🚀🚀</h1>
+			{footerElem}
+		</div>
+	);
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent/>);
