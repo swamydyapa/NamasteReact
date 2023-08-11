cce425ce6f5674eb4f4240419fdf22a0a8e34a79
swamydyapa <swamydyapa6.webdevp8@gmail.com>
2023-08-11 22:58:37 +0530

    Created ReactElement using JSX

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 const parent = React.createElement("div", { id: "parent" }, [
 	React.createElement("div", { id: "child" }, [
-		React.createElement("h1", {}, "This is Namaste React"),
+		React.createElement("h1", {}, "This is Namaste React🚀🚀"),
 		React.createElement("h2", {}, "by Swamy Dyapa"),
 	]),
 	React.createElement("div", { id: "child2" }, [
@@ -11,6 +11,19 @@ const parent = React.createElement("div", { id: "parent" }, [
 		React.createElement("h2", {}, "h2 from child2"),
 	]),
 ]);
-
+console.log("reactElement", parent);
+const jsxParent = (
+	<div id="parent">
+		<div id="child">
+			<h1> Hello React🚀🚀</h1>
+			<h2>by Swamy Dyapa</h2>
+		</div>
+		<div id="child2" className="child2 child">
+			<h1>h1 from child2</h1>
+			<h2>h2 from child2</h2>
+		</div>
+	</div>
+);
+console.log("jsxElement", jsxParent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxParent);
